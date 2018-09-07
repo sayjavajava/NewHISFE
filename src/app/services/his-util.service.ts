b45784b8ccde9c28ad16cbc22f8536f25e9b6153
Qari Jamal <jamal@solutiondots.net>
2018-09-07 10:14:32 +0500

    conflict

---
+++ src/app/services/his-util.service.ts
@@ -0,0 +1,33 @@
+import {Injectable} from '@angular/core';
+import 'rxjs/Rx';
+import {Router} from '@angular/router';
+
+@Injectable()
+export class HISUtilService {
+    staffID:Number;
+    patientId :Number;
+    constructor(private router: Router) {
+    };
+
+    tokenExpired(response: string) { 
+        if (response === 'invalid_token') {
+            window.localStorage.removeItem(btoa('access_token'));
+            window.localStorage.removeItem(btoa('refresh_token'));
+            window.localStorage.removeItem(btoa('expire_in'));
+            window.localStorage.removeItem(atob('permissions'));
+
+            this.router.navigate(['/login']);
+        }
+    }
+
+    hidePopupWithCloseButtonId(closeButtonId: string){
+        document.getElementById(closeButtonId).click();
+    }
+      staffId(id:number){
+        this.staffID=id;
+    }
+    setPatientId(id:number){
+        this.patientId=id;
+    }
+
+}
