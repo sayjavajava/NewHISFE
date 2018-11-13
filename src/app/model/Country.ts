ffb6eae113afe63d1627120ae83da67ea20c8414
hamza hamid <hamza.hamid@sdots.local>
2018-11-13 17:48:46 +0500

    comitting org

---
+++ src/app/model/Country.ts
@@ -0,0 +1,16 @@
+export class Country{
+
+    name: string;
+    iso3: string;
+    iso2: string;
+    countryCode: string;
+    phoneCode:string;
+    capital:string;
+    status:string;
+
+    constructor() {
+    }
+
+
+
+}
