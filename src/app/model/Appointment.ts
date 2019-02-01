4b43f733a5e73f177b89554b6543755e812c68cd
hamza hamid <hamza.hamid@sdots.local>
2019-02-01 17:18:38 +0500

    resolved

--- src/app/model/Appointment.ts
+++ src/app/model/Appointment.ts
@@ -48,9 +48,8 @@ export class Appointment{
     
     constructor(id?:number,appointmentId?:string,title ?: string, branchId ?: number, doctorId ?: number,scheduleDateAndTime ?:DateTimeFormat, scheduleDate?: string, end?: string, draggable?: boolean,selectedRecurringDays ?:any[], appointmentType?: any[], notes?: string, patient?: number, reason?:
          string, statusId?: number, duration?: number, followUpDate?: Date, followUpReason?: string ,followUpReminder?:boolean,recurringAppointment?:boolean,recurseEvery?:string
-        ,firstAppointment?:Date,lastAppointment?:Date,examRoom?:number,age?:string,cellPhone?:string,gender?:string,email?:string,color ?:string,roomId?:number,newPatient ?:string,dob?:Date,serviceId?:number,stateOfPatientBox?:boolean,dateSchedule?:Date
-
-    ) {
+        ,firstAppointment?:Date,lastAppointment?:Date,examRoom?:number,age?:string,cellPhone?:string,gender?:string,email?:string,color ?:string,roomId?:number,newPatient ?:string,dob?:Date,serviceId?:number,stateOfPatientBox?:boolean,dateSchedule?:Date,apptType?:any)
+         {
         this.id = id;
         this.appointmentId = appointmentId;
         this.title = title;
@@ -71,7 +70,7 @@ export class Appointment{
         this.followUpReason = followUpReason;
         this.followUpReminder=followUpReminder;
         this.recurringAppointment=recurringAppointment;
-        this.recurseEvery=recurseEvery;
+     //   this.recurseEvery=recurseEvery;
         this.firstAppointment= new Date(firstAppointment);
         this.lastAppointment= new Date(lastAppointment);
         this.examRoom=examRoom;
